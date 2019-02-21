2897419f16350c094fe245013872c3494700a4c4
JaJedi <47127875+JaJedi@users.noreply.github.com>
2019-02-20 18:42:53 -0600

    Updated slightly
    We've made the robot drive farther in order to push the firetruck farther.

--- main.c
+++ main.c
@@ -5,16 +5,16 @@ int main()
     enable_servos();
     cmpc(0);
     set_servo_position(0,280);
-    set_servo_position(2,1500);
+    set_servo_position(2,1600);
     msleep(1000);
     set_servo_position(0,1360);
     msleep(1000);
-    set_servo_position(2,760);
+    set_servo_position(2,1200);
     msleep(1000);
     set_servo_position(0,1300);
     while (1)
     {
-        if (gmpc(0)<6188)
+        if (gmpc(0)<6688)
         {
             motor(0,75);
             motor(2,60);
@@ -24,6 +24,7 @@ int main()
             break;
         }
     }
-    set_servo_position(2,1360);
+    set_servo_position(2,1500);
+    
     return 0;
 }
